4b88cec45aee6a36a5ce55f61dd6ebb5efd3cc0b
KuntimaKiala <kuntima.kiala@gmail.com>
2018-08-02 11:24:01 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,75 +1,17 @@
 #include <iostream>
-#include <cstdint>
-#include <inttypes.h>
+#include "DialogueTree.h"
+
 
 using namespace std;
 
+int main()
+{
 
-void addOne(int *myPointer) ;
-void addTwo(int &integer);
-void addThree(int a) ;
-int main(){
-
-    // Stack
-    int a = 5 ;
-    int *pointer = nullptr ;
-    pointer = &a ;
-
-
-    cout << &*pointer << endl ;
-    cout << *pointer << endl ;
-    cout << &a << endl ;
-    addOne( pointer) ;
-    //a = 10 ;
-    a+=1 ;
-    cout << *pointer << endl ;
-    cout << pointer << endl ;
-    cout << &a << endl ;
-
-
-
-    // Heap
-
- int *myPointer = nullptr ;
- myPointer = new int[1000];
-cout << myPointer << endl ;
- delete[] myPointer ;
-
-
-int *MyPointer = nullptr ;
-MyPointer = new int ;
-*MyPointer = a*2;
-addOne(MyPointer) ;
-cout << *MyPointer << endl ;
-delete MyPointer ;
-
-
-
-int integer = 0 ;
-cout << integer << endl ;
-addTwo(integer) ;
-cout << integer << endl ;
-
-int valor = 0 ;
-cout << valor << endl;
-addThree(valor) ;
-cout << valor << endl;
-
+    DialogueTree dialogueTree ;
+    dialogueTree.init() ;
+    int rv = dialogueTree.performDialogue() ;
+    if (rv == 1) {
+        cout << "\nMy Courageous Brave Warrior, Let start Then" << endl ;
+    }
     return 0;
-}
-
-
-void addOne(int *myPointer){
-    *myPointer += 5 ;
-}
-
-
-void addTwo (int &integer){
-
- integer += 2 ;
-}
-
-
-void addThree(int a ) {
- a+=3 ;
 }
